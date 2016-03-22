478663c8b61cbe948ac345e4636f085bcb059a29
Antonio Carlos Mantovani <antonio.mantovani@gmail.com>
2016-03-22 11:00:43 -0300

    testando pending changes

--- src/MvcMovie/Models/Movie.cs
+++ src/MvcMovie/Models/Movie.cs
@@ -10,7 +10,8 @@ namespace MvcMovie.Models
         [StringLength(60,MinimumLength =3)]
         public string Title { get; set; }
 
-        [Display(Name="Release Date") , DataType(DataType.Date)]
+        [Display(Name="Release Date")]
+        [ DataType(DataType.Date)]
         public DateTime ReleaseDate { get; set; }
 
         [RegularExpression(@"^[A-Z]+[a-zA-Z''-'\s]*$") , Required , StringLength(30)]
